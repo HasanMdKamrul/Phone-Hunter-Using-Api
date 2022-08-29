e1240a6a17396a3d24c9404cd11d25902588b059
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-08-29 22:39:12 +0200

    Show All functionality added

--- js/phonehunter.js
+++ js/phonehunter.js
@@ -7,20 +7,12 @@ const loaderToggler = isLoading =>{
 
 // ** Load Phones data
 
-const loadPhonesData = async (search)=>{
+const loadPhonesData = async (search,dataLimit)=>{
     try {
-        if (search) {
-            const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`);
-            response.ok ? console.log('Successful') : console.log('Unsuccessful');
-            const data = await response.json();
-            displayPhones(data)
-        } else {
-            loaderToggler(true)
-            const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=iPhone`);
-            response.ok ? console.log('Successful') : console.log('Unsuccessful');
-            const data = await response.json();
-            displayPhones(data)
-        }
+        const response = await fetch(`https://openapi.programming-hero.com/api/phones?search=${search}`);
+        response.ok ? console.log('Successful') : console.log('Unsuccessful');
+        const data = await response.json();
+        displayPhones(data,dataLimit)
         
     } catch (error) {
         console.log(error);
@@ -30,7 +22,7 @@ const loadPhonesData = async (search)=>{
 
 // ** Display phones as card components
 
-const displayPhones = data => {
+const displayPhones = (data,dataLimit) => {
     // ** Where to display phones
 
     const phonesContainer = document.getElementById('phone-container');
@@ -39,7 +31,9 @@ const displayPhones = data => {
 
     // ** get Data to show in ui
 
-    const {data:phones} = data;
+    let {data:phones} = data;
+
+    
 
     // ** When no phones data found
 
@@ -49,10 +43,22 @@ const displayPhones = data => {
 
     phones.length === 0 ? noDataContainer.classList.remove('d-none') : noDataContainer.classList.add('d-none')
 
+    // ** ShowAll button functinality
+
+    const showAllContainer = document.getElementById('show-all');
+    console.log(phones.length)
+
+    if (phones.length > 10 && dataLimit) {
+        phones = phones.slice(0,dataLimit);
+        showAllContainer.classList.remove('d-none');
+    } else{
+        showAllContainer.classList.add('d-none');
+    }
+    console.log("phones:",phones)
     // ** Display phones only 10
 
-    phones.slice(0,10).forEach(phone => {
-        // console.log(phone)
+    phones.forEach(phone => {
+        console.log(phone)
         const {brand,image,phone_name} = phone;
         const divContent = document.createElement('div');
         divContent.classList.add('col');
@@ -84,9 +90,17 @@ document.getElementById('button-addon2').addEventListener('click',()=>{
     // ** loader starts here
     loaderToggler(true);
     const searchText = document.getElementById('search-input').value;
-    loadPhonesData(searchText);
-    document.getElementById('search-input').value = '';
+    loadPhonesData(searchText,20);
+    // document.getElementById('search-input').value = '';
 });
 
-loadPhonesData();
+// loadPhonesData();
 
+// ** showAll button
+
+document.getElementById('btn-show-all').addEventListener('click',()=>{
+    loaderToggler(true);
+    const searchText = document.getElementById('search-input').value;
+    loadPhonesData(searchText);
+    // document.getElementById('search-input').value = '';
+})
