1d65e0810f363b5ab0c488fc596fca279ae98d53
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-08-29 19:39:09 +0200

    Phones are displayed using cards

--- js/phonehunter.js
+++ js/phonehunter.js
@@ -15,9 +15,37 @@ const loadPhonesData = async ()=>{
 // ** Display phones as card components
 
 const displayPhones = data => {
+    // ** Where to display phones
+
+    const phonesContainer = document.getElementById('phone-container');
+
+    // ** get Data to show in ui
+
     const {data:phones} = data;
 
-    phones.forEach(phone => console.log(phone))
+    phones.forEach(phone => {
+        // console.log(phone)
+        const {brand,image,phone_name} = phone;
+
+        const divContent = document.createElement('div');
+
+        divContent.classList.add('col');
+        divContent.classList.add('rounded-lg');
+        divContent.innerHTML = `
+        <div class="card shadow-lg">
+        <img src="${image}" class="card-img-top p-3 rounded" alt="..." />
+            <div class="card-body">
+            <h5 class="card-title text-warning">${brand}</h5>
+            <p class="card-text">
+                ${phone_name}
+            </p>
+            </div>
+        </div>
+        `;
+
+        phonesContainer.appendChild(divContent);
+
+    })
 }
 
 loadPhonesData();
